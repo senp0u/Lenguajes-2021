af2a261292060a2e9c544d91b07462df125f2fee
Senp0u <josue.ad.95@gmail.com>
2021-01-22 02:44:05 -0800

    Consumiendo api de Issue

---
+++ HelpDesk-Support/HelpDesk-SupportAPI/Controllers/IssueController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HelpDesk_SupportAPI.Models;
+
+namespace HelpDesk_SupportAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+    {
+        private readonly DB_A6E1ED_lenguajesContext _context;
+
+        public IssueController()
+        {
+            _context = new DB_A6E1ED_lenguajesContext();
+        }
+
+        // GET: api/Issue
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Issue>>> GetIssue()
+        {
+            return await _context.Issue.ToListAsync();
+        }
+
+        // GET: api/Issue/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Issue>> GetIssue(int id)
+        {
+            var issue = await _context.Issue.FindAsync(id);
+
+            if (issue == null)
+            {
+                return NotFound();
+            }
+
+            return issue;
+        }
+
+        // PUT: api/Issue/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutIssue(int id, Issue issue)
+        {
+            if (id != issue.ReportNumber)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(issue).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!IssueExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Issue
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Issue>> PostIssue(Issue issue)
+        {
+            _context.Issue.Add(issue);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetIssue", new { id = issue.ReportNumber }, issue);
+        }
+
+        // DELETE: api/Issue/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Issue>> DeleteIssue(int id)
+        {
+            var issue = await _context.Issue.FindAsync(id);
+            if (issue == null)
+            {
+                return NotFound();
+            }
+
+            _context.Issue.Remove(issue);
+            await _context.SaveChangesAsync();
+
+            return issue;
+        }
+
+        private bool IssueExists(int id)
+        {
+            return _context.Issue.Any(e => e.ReportNumber == id);
+        }
+    }
+}
