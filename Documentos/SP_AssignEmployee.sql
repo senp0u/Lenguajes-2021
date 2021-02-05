4014bee77d08754a451f469ced61458d9d09244c
Senp0u <josue.ad.95@gmail.com>
2021-02-05 04:34:11 -0800

    Actualizando SP

--- Documentos/SP_AssignEmployee.sql
+++ Documentos/SP_AssignEmployee.sql
@@ -1,12 +1,16 @@
 CREATE PROCEDURE AssignEmployee
 	@ReportNumber int,
-	@EmployeeID int
+	@EmployeeID int,
+	@SupervisorId int
 AS
 BEGIN
     -- Insert statements for procedure here
 	UPDATE Issue SET
-	[EmployeeId] = @EmployeeID
-	WHERE [ReportNumber] = @ReportNumber
+	[EmployeeId] = @EmployeeID,
+	[SupervisorId] = @SupervisorId,
+	[ModifyBy] = @SupervisorId,
+	[ModifyDate] = GETDATE()
+ 	WHERE [ReportNumber] = @ReportNumber
 
 END
 GO
