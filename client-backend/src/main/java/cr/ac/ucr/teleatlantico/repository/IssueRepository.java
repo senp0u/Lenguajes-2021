d625373d1450352e6d2ece22a7bad63450228a49
Senp0u <josue.ad.95@gmail.com>
2021-02-19 04:45:04 -0800

    Cambios multiples

--- client-backend/src/main/java/cr/ac/ucr/teleatlantico/repository/IssueRepository.java
+++ client-backend/src/main/java/cr/ac/ucr/teleatlantico/repository/IssueRepository.java
@@ -1,11 +1,15 @@
 package cr.ac.ucr.teleatlantico.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import cr.ac.ucr.teleatlantico.domain.Issue;
 
 @Repository
 public interface IssueRepository extends JpaRepository<Issue, Integer>{
-
+	
+	@Query(value = "{ call SelectIssueByIdClient(?1)}", nativeQuery = true)
+    Issue getIssueByIdClient(@Param("@ClientId") Integer ClientId);
 }
--- client-backend/src/main/java/cr/ac/ucr/teleatlantico/service/IssueService.java
+++ client-backend/src/main/java/cr/ac/ucr/teleatlantico/service/IssueService.java
@@ -5,6 +5,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import cr.ac.ucr.teleatlantico.domain.Issue;
 import cr.ac.ucr.teleatlantico.repository.IssueRepository;
 
 @Service
@@ -14,5 +15,11 @@ public class IssueService {
 	@Autowired
 	private IssueRepository repository;
 	
-	
+	public Issue getIssueByClientId(int id) {
+		return repository.getIssueByIdClient(id);
+	}
+
+	public Issue save(Issue issue) {
+		return repository.save(issue);
+	}
 }
